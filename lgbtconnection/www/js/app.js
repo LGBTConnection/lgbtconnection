8b2c3075dfc0771ca6eb10ac0eb0df3a43588c5c
metacrektal <vdminh.lbt.91@gmail.com>
2017-03-09 20:28:07 +0700

    Tao template + app.js

--- lgbtconnection/www/js/app.js
+++ lgbtconnection/www/js/app.js
@@ -3,6 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+var messages = ['abc','bdf'];
 angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
@@ -21,4 +22,133 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+})
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+     .state('signin', {
+      url: '/sign-in',
+      templateUrl: 'templates/SignInTemplate.html',
+      controller: 'SignInCtrl'
+    })
+    .state('signup',{
+      url: '/sign-up',
+      templateUrl: 'templates/SignUpTemplate.html',
+      controller: 'SignUpCtrl'
+    })
+    .state('tabs.chat',{
+      url: '/chat/:_idUser',
+      views: {
+        'friend-tab': {
+          templateUrl: 'templates/ChatTemplate.html',
+          controller: 'ChatCtrl'
+        }
+      }
+    })
+    .state('tabs', {
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'templates/MainTemplate.html',
+      controller: 'MainCtrl'
+    })
+    .state('tabs.home', {
+      url: '/home',
+      views: {
+        'home-tab': {
+          templateUrl: 'templates/HomeTemplate.html',
+          controller: 'HomeTabCtrl'
+        }
+      }
+    })
+    .state('tabs.friend', {
+      url: '/friend',
+      views: {
+        'friend-tab': {
+          templateUrl: 'templates/FriendTemplate.html',
+          controller: 'FriendTabCtrl'
+        }
+      }
+    })
+    .state('tabs.setting', {
+      url: '/setting',
+      views: {
+        'setting-tab': {
+          templateUrl: 'templates/SettingTemplate.html',
+          controller: 'SettingTabCtrl'
+        }
+      }
+    })
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/sign-in');
+
+})
+.controller('SignInCtrl', function($scope, $state){
+  $scope.signIn = function(user){
+    $state.go('tabs.home');
+  }
+})
+.controller('SignUpCtrl', function($scope){
+   $scope.signUp = function(user){
+    
+  }
+})
+.controller('MainCtrl', function($scope){
+  
+})
+.controller('HomeTabCtrl', function($scope){
+
+})
+.controller('FriendTabCtrl', function($scope){
+
+})
+.controller('SettingTabCtrl', function($scope){
+
+})
+.controller('ChatCtrl', function($scope, $stateParams, $timeout, $ionicScrollDelegate){
+ // alert($stateParams._idUser);
+ $scope.showTime = true;
+
+  $scope.focusManager = { focusInputOnBlur: true };
+
+  var alternate;
+
+
+  $scope.getItemHeight = function(event) {
+    console.log('getItemHeight', event);
+    return 300;
+  }
+
+  $scope.sendMessage = function() {
+    alternate = !alternate;
+
+    var d = new Date();
+    d = d.toLocaleTimeString().replace(/:\d+ /, ' ');
+
+    $scope.messages.push({
+      userId: '12345',
+      text: $scope.data.message,
+      time: d
+    });
+    $scope.messages.push({
+        userId: $stateParams._idUser,
+        text: $scope.data.message + " Guest",
+        time: d
+    })
+
+    delete $scope.data.message;
+    $ionicScrollDelegate.scrollBottom(true);
+
+  };
+
+
+  $scope.data = {};
+  $scope.myId = '12345';
+  $scope.messages = [];
 })
